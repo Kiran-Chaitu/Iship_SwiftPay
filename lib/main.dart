3955a831a85031223ca95d64904747d3f11fa853
Kiran-Chaitu <119839377+Kiran-Chaitu@users.noreply.github.com>
2024-08-13 10:07:25 +0530

    kotlin Version Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
+import 'package:SwiftPay/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:SwiftPay/starting_page.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp( const MyApp());
 }
 
